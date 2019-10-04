2e946b055d30a86f706b0f1fd026c64f2d540c6e
Volodsher <volodsher@gmail.com>
2019-10-04 12:13:25 +0300

    Updates

---
+++ precache-manifest.144934311a1f30e0c2471382f694b621.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "85d402af193fd60bdc0e3c0884895484",
+    "url": "/react_tabs-with-router/index.html"
+  },
+  {
+    "revision": "8c2b1b549c581ffac40c",
+    "url": "/react_tabs-with-router/static/css/main.cf0f3302.chunk.css"
+  },
+  {
+    "revision": "61c695c2cb09b7f480b8",
+    "url": "/react_tabs-with-router/static/js/2.af97e3c8.chunk.js"
+  },
+  {
+    "revision": "8c2b1b549c581ffac40c",
+    "url": "/react_tabs-with-router/static/js/main.ce49a1d1.chunk.js"
+  },
+  {
+    "revision": "da3ca6fceb301548d22d",
+    "url": "/react_tabs-with-router/static/js/runtime~main.5858125e.js"
+  }
+]);
